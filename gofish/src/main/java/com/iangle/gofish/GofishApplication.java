d93726b27b2758c5b409f9521c25a6c445ae49ef
Isaac Angle <iangle22@my.whitworth.edu>
2020-05-23 21:14:40 -0700

    added some initial files

---
+++ gofish/src/main/java/com/iangle/gofish/GofishApplication.java
@@ -0,0 +1,13 @@
+package com.iangle.gofish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofishApplication.class, args);
+	}
+
+}
---
+++ gofish/src/test/java/com/iangle/gofish/GofishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iangle.gofish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
